78a7ed1c022bc566d861c75a50ee3a15a696f994
Punit Lad <punitlad@gmail.com>
2026-03-09 18:43:24 -0400

    update to validate findings

--- render-template.sh
+++ render-template.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
-aws accessanalyzer validate-policy --policy-document file://$1 --policy-type RESOURCE_POLICY
+FINDINGS=$(aws accessanalyzer validate-policy --policy-document file://policy.json --policy-type RESOURCE_POLICY)
+FINDINGS_COUNT=$(echo $FINDINGS | jq '.findings | length')
+
+if [[ "$FINDINGS_COUNT" -gt 0 ]]; then
+  echo "Found $FINDINGS_COUNT policy issue(s):"
+  echo $FINDINGS | jq .
+  exit 1
+fi
 
 cfn-flip policy.json policy.yaml
-
-rain pkg iam-stack.yaml
+rain pkg iam-stack.yaml --output iam-stack-rendered.yaml
