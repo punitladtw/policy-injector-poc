d284322f10c1920d661f3bb310659be34b789751
Punit Lad <punitlad@gmail.com>
2026-03-09 18:50:20 -0400

    add invalid policy to test again; create gha for testing

--- render-template.sh
+++ render-template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-FINDINGS=$(aws accessanalyzer validate-policy --policy-document file://policy.json --policy-type RESOURCE_POLICY)
+FINDINGS=$(aws accessanalyzer validate-policy --policy-document file://$1 --policy-type RESOURCE_POLICY)
 FINDINGS_COUNT=$(echo $FINDINGS | jq '.findings | length')
 
 if [[ "$FINDINGS_COUNT" -gt 0 ]]; then
@@ -12,3 +12,6 @@ fi
 
 cfn-flip policy.json policy.yaml
 rain pkg iam-stack.yaml --output iam-stack-rendered.yaml
+
+echo "Successfully rendered cloud formation template"
+cat iam-stack-rendered.yaml
